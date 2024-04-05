e85e7e695349552d0aaea59460662b0399f5f7f8
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-04-05 19:43:01 -0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+
+export default function Home() {
+    return (
+        <View >
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
